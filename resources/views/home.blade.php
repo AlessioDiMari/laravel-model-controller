59a1e532fa26877c9d75b730d553e34b19423132
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-04-10 18:36:07 +0200

    stilizzazione in cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,30 @@
     <div class="container py-5">
         <h1>Lista dei film</h1>
 
-        {{$movies}}
+        
+        <div class="row row-cols-auto g-5 py-5">
+
+            @foreach ($movies as $movie)
+            
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                          <h5 class="card-title">{{$movie->title}}</h5>
+                          <h6>{{$movie->original_title}}</h6>
+                          <p class="card-text">
+                            Nationalità: {{$movie->nationality}} <br>
+                            Data di uscita: {{$movie->date}} <br>
+                            Voto: {{$movie->vote}}
+                          </p>
+                          <a href="#" class="btn btn-success">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+
+            @endforeach
+
+        </div>
+
     </div>
 
 @endsection
